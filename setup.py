2cc723c21f1b5401d838b36e43cf489f18c0d507
dr4Ke <dr4ke@dr4ke.net>
2010-07-15 10:32:10 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+version='1.0'
+
+setup(name='TracGitosis',
+      version=version,
+      description="user's ssh public key for use with gitosis",
+      author='Christophe Drevet',
+      author_email='dr4ke@dr4ke.net',
+      url='',
+      keywords='trac gitosis plugin',
+      license="GPLv3+",
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
+      include_package_data=True,
+      package_data={ 'tracgitosis': ['templates/*', 'htdocs/*'] },
+      zip_safe=False,
+      entry_points = """
+      [trac.plugins]
+      tracgitosis = tracgitosis
+      """,
+      )
+
---
+++ tracgitosis/__init__.py
@@ -0,0 +1,2 @@
+# 
+from tracgitosisprefs import *
